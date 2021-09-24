52d815794ff5544d02a30ce0b26525c5293bd10f
Timxxx <tbenker@gmx.ch>
2021-09-24 16:38:14 +0200

    updated urltester2

--- urltester2.py
+++ urltester2.py
@@ -12,71 +12,39 @@ webhook = ""
 
 ###############################
 
-
-
 chars = "abcdefghijklmnopqrstuvwxyz1234567890"
 urls = []
 valid = []
 
-count = input("Count of urls to test: ")
-if(not count.isdigit()):
-    count = 10
-count = int(count)
-
 # validate url
 if url[len(url)-1] != "/": url += "/" 
-
 index = 0
 
 def sendToWebhook(_url):
     if webhook != "":
         data = {"content": _url}
         response = requests.post(webhook, json=data)
-        print(f"Url got sent to to webhook ({response.status_code}) | {_url}")
+        print(f"[{len(urls)}] Url got sent to to webhook ({response.status_code}) | {_url}")
 
 def generateRandUrl(length=5):
     fileName = "".join(random.choice(chars) for i in range(length))
-    return url + fileName + ext
+    _url = url + fileName + ext
+    if _url in urls:
+        _url = generateRandUrl(length)
+    return _url
 
-def getRequest(_url):
-    session = FuturesSession()
-    head = session.head(_url)
-    if(head.result().status_code==200): 
+def isValid(_url):
+    request = requests.head(_url)
+    return request.status_code == 200
+
+def testUrl(_url):
+    urls.append(_url)
+    if isValid(_url):
         sendToWebhook(_url)
+        valid.append(_url)
     else:
-        print(f"Nothing on: {_url}")
-    return head
-
-def getStatusCode(request):
-    try:
-        return request.result().status_code
-    except:
-        return 408
-
-print("Generating urls...")
-
-# generate list urls
-for i in range(count):
-    _url = generateRandUrl(fileNameLength)
-    if _url not in urls and validators.url(_url):
-        urls.append(_url)
-        print(f"generated ({i+1}): {_url}")
+        print(f"[{len(urls)}] Nothing on {_url}")
 
 print("Started to search...")
-
-# make a request for every url
-df = pd.DataFrame({"url": urls})
-df["statusCode"] = df["url"].apply(getRequest).apply(getStatusCode)
-
-# get the valid urls
-df.set_index(["statusCode"])
-for _url in df.loc[df["statusCode"]==200].values:
-    valid.append(_url[0])
-
-print(valid)
-
-stats = df.groupby('statusCode')["url"].count().reset_index()
-print(stats)
-
-_ = "is" if len(valid)==1 else "are"
-print(f"\nTested {len(urls)} urls, {len(valid)} {_} valid")
+while True:
+    testUrl(generateRandUrl(fileNameLength))
